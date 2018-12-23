8f5419b9b3810953ea6e88fc6fafe03ecb18c6df
Artem Borisov <dedsa2002@gmail.com>
2018-12-23 16:01:49 +0400

    msm8953-common: Enforce RRO only for framework-res
    Change-Id: I8432cec37860864bd78591403f338fd029357b39

--- device.mk
+++ device.mk
@@ -22,6 +22,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_ENFORCE_RRO_TARGETS := \
+    framework-res
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
